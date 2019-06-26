04166d5afb4b96524446307cbc3038bea8b3df89
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-06-26 18:07:19 +0200

    trying to fix redirect

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     beforeModel(/* transition */) {
-        this.replaceWith('dashboard'); // Implicitly aborts the on-going transition.
+        this.replaceWith('login'); // Implicitly aborts the on-going transition.
     }
 });
