bcb5b37bbe67e5013ab0cbef789b7b0d0ee9a82e
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-06-26 14:56:34 +0200

    add loading spinner

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,16 +1,20 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { set } from '@ember/object';
 
 export default Controller.extend({
   media: service(),
   session: service(),
   toastr: service('toast'),
+  activeLoader: false,
 
   actions: {
     authenticate() {
       let { identification, password } = this.getProperties('identification', 'password');
+      set(this, 'activeLoader', true);
       this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
         this.set('errorMessage', reason.error || reason);
+        set(this, 'activeLoader', false);
         this.toastr.error("Password or Email is wrong", "Error");
       });
     }
